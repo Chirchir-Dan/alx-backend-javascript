ce9bb9a4c7a3aad5f5463f070b6b807bd0713397
Chirchir-Dan <1danchirchir@gmail.com>
2024-10-08 23:15:46 +0300

    file 2 using filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+const getStudentsByLocation = (students, city) => {
+  if (!Array.isArray(students) || typeof city !== 'string') {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
+};
+
+export default getStudentsByLocation;
