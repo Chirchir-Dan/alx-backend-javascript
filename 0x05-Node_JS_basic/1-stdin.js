8f8fb8060647cbf072e59768d0f49118c1c06714
Chirchir-Dan <1danchirchir@gmail.com>
2024-11-26 21:42:57 +0300

    refactored 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,16 @@
-#!/usr/bin/env node
+[Oprocess.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk.toString().trim()}\n`);
-    if (process.stdin.isTTY) {
-      process.exit(0); // Exit immediately for interactive input
-    }
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
